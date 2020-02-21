a897d04dd65abbacc93f9f175935bfc12ffab3cb
edynak-demo <edynakdemo@gmail.com>
2020-02-21 14:01:26 -0500

    implemented image defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,58 +1,59 @@
 class PortfoliosController < ApplicationController
-	def index
-		@portfolio_items = Portfolio.all
-	end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
   def angular
     @angular_portfolio_items = Portfolio.angular
-	def new
-		@portfolio_item = Portfolio.new
-	end
+  end
 
-	def create
-		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
-		respond_to do |format|
-			if @portfolio_item.save
-				format.html { redirect_to @portfolios_path, notice: 'Your Portfolio item is now live..'}
-			else
-				format.html { render :new }
-			end
-		end
-	end
-	def edit
-		@portfolio_item = Portfolio.find(params[:id])
-	end
-	def update
-		@portfolio_item = Portfolio.find(params[:id])
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'Your record was successfully updated...' }
-        format.json { render :show, status: :ok, location: @tutorial }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @tutorial.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def show
-  	@portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-  	# Perform the lookup
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
 
-  	@portfolio_item = Portfolio.find(params[:id])
-
-  	# Destroy/delete the record
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Your record was successfully deleted and is now permanently offline...' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-end
+
 end

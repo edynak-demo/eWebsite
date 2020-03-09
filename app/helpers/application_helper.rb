bfe2b7fcd184a182f41e48f65fc0591ec7c80ee4
edynak-demo <edynakdemo@gmail.com>
2020-03-09 12:17:13 -0400

    implemented refactor for navigation and for active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,55 @@ module ApplicationHelper
   def copyright_generator
     EdynakViewTool::Renderer.copyright 'Christopher Edynak', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About'
+      },
+      {
+        url: news_path,
+        title: 'News'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+      {
+        url: tutorials_path,
+        title: 'Tutorials'
+      },
+      {
+        url: services_path,
+        title: 'Services'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: store_path,
+        title: 'Store'
+      },
+    ]
+  end
+
+  def nav_generator style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end

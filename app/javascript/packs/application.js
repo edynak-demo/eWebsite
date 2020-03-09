14966a05187b53aa6278ba827c4dc8e5fba91a9c
edynak-demo <edynakdemo@gmail.com>
2020-03-09 14:26:03 -0400

    integrated inital drag and drop implementation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,16 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("jquery-ui")
 require("bootstrap")
 require("packs/html.sortable")
+
+
+
+$(function() {
+	$('.sortable').sortable();
+});
+
 
 
 
